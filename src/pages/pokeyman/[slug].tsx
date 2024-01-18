2448d1196c3a49114702a4d5db25de31d623e35f
Jeff Darchuk <jeffd@uniform.dev>
2024-01-18 07:36:38 -0800

    all pokey

--- src/pages/pokeyman/[slug].tsx
+++ src/pages/pokeyman/[slug].tsx
@@ -1,13 +1,14 @@
 import JeffHeader from "@/components/header";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import Head from "next/head";
-import React from "react";
+import React, { use } from "react";
 import Image from "next/image";
 import "./../../app/globals.css";
 import Link from "next/link";
 
 interface PokeyListData {
-  results: { name: string; url: string }[];
+  results: { name: string }[];
+  next?: string;
 }
 interface PokeyData {
   name: string;
@@ -30,35 +31,37 @@ const PokeyPage = ({
       <JeffHeader />
       <div className="grid grid-cols-12">
         <div className="col-span-2 row-start-1">
-          {list?.results.map((result) => {
+          {list?.map((result) => {
             return (
               <div key={result.name}>
-                <Link href={`/pokeyman/${result.name}`}>{result.name}</Link>
+                <Link href={`/pokeyman/${result.name}`} scroll={false}>
+                  {result.name}
+                </Link>
               </div>
             );
           })}
         </div>
         <div className="col-span-9 row-start-1 -mt-16">
-          <h1> {pokemon?.name}</h1>{" "}
-          <Image
-            src={pokemon?.sprites.front_default ?? ""}
-            alt="pokey"
-            width={128}
-            height={128}
-          ></Image>
+          <div className="fixed">
+            <h1> {pokemon?.name}</h1>
+            <Image
+              src={pokemon?.sprites.front_default ?? ""}
+              alt="pokey"
+              width={128}
+              height={128}
+            ></Image>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 export async function getStaticPaths() {
-  const pokeListFetch = await fetch("https://pokeapi.co/api/v2/pokemon");
-  const pokeListData = (await pokeListFetch.json()) as PokeyListData;
+  const allPokemon = await getAllPokemon();
   return {
-    paths: pokeListData.results.map((pokeListDataItem) => ({
+    paths: allPokemon.map((pokeListDataItem) => ({
       params: {
         slug: pokeListDataItem.name,
-        data: pokeListDataItem.url,
       },
     })),
     fallback: true,
@@ -76,10 +79,40 @@ export async function getStaticProps(
       `https://pokeapi.co/api/v2/pokemon/${ctx.params.slug}`
     );
     const pokeData = (await pokeFetch.json()) as PokeyData;
-    const pokeListFetch = await fetch("https://pokeapi.co/api/v2/pokemon");
-    const pokeListData = (await pokeListFetch.json()) as PokeyListData;
-    return { props: { pokemon: pokeData, list: pokeListData } };
+    return {
+      props: {
+        pokemon: {
+          name: pokeData.name,
+          sprites: { front_default: pokeData.sprites.front_default },
+        },
+        list: await getAllPokemon(),
+      },
+    };
   }
-  return { props: { pokemon: null } };
+  return {
+    props: {
+      pokemon: { name: "n/a", sprites: { front_default: "n/a" } },
+      list: await getAllPokemon(),
+    },
+  };
 }
+
+async function getAllPokemon() {
+  const pokeListFetch = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const pokeListData = (await pokeListFetch.json()) as PokeyListData;
+  const ret: PokeyListData["results"] = [];
+  const s = [pokeListData];
+  while (s.length > 0) {
+    const listData = s.pop();
+    listData?.results.forEach((data) => {
+      ret.push({ name: data.name });
+    });
+    if (listData?.next) {
+      const nextFetch = await fetch(listData.next);
+      s.push((await nextFetch.json()) as PokeyListData);
+    }
+  }
+  return ret;
+}
+
 export default PokeyPage;
